df1ee28d51af06763963d5c5ddaa2d60dd7dc293
Paweł Twardziak <ptwardziak@eversis.com>
2019-11-24 23:14:53 +0100

    Improve transition on change route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
-import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Component, HostBinding, OnDestroy, OnInit} from '@angular/core';
+import {
+    Event,
+    NavigationCancel,
+    NavigationEnd,
+    NavigationError,
+    NavigationStart,
+    Router,
+} from '@angular/router';
+import {of, Subject, timer} from 'rxjs';
+import {delayWhen, filter, takeUntil} from 'rxjs/operators';
 
 import {LoaderService} from './services/loader/loader.service';
-import {Subject} from 'rxjs';
-import {takeUntil} from 'rxjs/operators';
+import {insideWorkspace} from '@angular/cli/utilities/project';
 
 @Component({
     selector: 'app-root',
@@ -14,15 +23,40 @@ export class AppComponent implements OnInit, OnDestroy {
 
     public showLoader = false;
 
+    public hide = false;
+
     private destroyed$ = new Subject<void>();
 
-    constructor(private loaderService: LoaderService) {}
+    constructor(private loaderService: LoaderService, private router: Router) {}
 
     ngOnInit(): void {
         this.loaderService
             .get()
             .pipe(takeUntil(this.destroyed$))
             .subscribe(show => (this.showLoader = show));
+        this.router.events
+            .pipe(
+                filter((event: Event) => {
+                    return event instanceof NavigationStart || event instanceof NavigationEnd;
+                }),
+                delayWhen(
+                    (
+                        event: NavigationStart | NavigationEnd | NavigationCancel | NavigationError,
+                    ) => {
+                        if (event instanceof NavigationStart) {
+                            return of(0);
+                        }
+                        return timer(150);
+                    },
+                ),
+            )
+            .subscribe((event: NavigationStart | NavigationEnd) => {
+                if (event instanceof NavigationStart) {
+                    this.hide = true;
+                } else {
+                    this.hide = false;
+                }
+            });
     }
 
     ngOnDestroy(): void {
