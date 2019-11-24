78bb2b8144dc6b1beed6731992fa26662c041c30
Paweł Twardziak <ptwardziak@eversis.com>
2019-11-24 20:13:41 +0100

    Remove Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HeaderComponent} from './components/header/header.component';
 import {NavComponent} from './components/header/nav/nav.component';
 import {NotFoundComponent} from './components/not-found/not-found.component';
@@ -13,7 +12,7 @@ import {AppInterceptor} from './app-interceptor';
 
 @NgModule({
     declarations: [AppComponent, HeaderComponent, NavComponent, NotFoundComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule],
     providers: [{provide: HTTP_INTERCEPTORS, useClass: AppInterceptor, multi: true}],
     bootstrap: [AppComponent],
 })
